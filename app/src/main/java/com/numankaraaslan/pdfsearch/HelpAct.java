c2720aa1a6f488a6dcc9e646a4ca982ac83ddbe7
Numan Karaaslan <numan4634@hotmail.com>
2020-11-01 23:33:27 +0300

    Android 10 support

--- app/src/main/java/com/numankaraaslan/pdfsearch/HelpAct.java
+++ app/src/main/java/com/numankaraaslan/pdfsearch/HelpAct.java
@@ -1,7 +1,10 @@
 package com.numankaraaslan.pdfsearch;
 
+import android.content.*;
+import android.net.*;
 import android.os.*;
 import android.view.*;
+import android.widget.*;
 
 import androidx.appcompat.app.*;
 
@@ -16,6 +19,24 @@ public class HelpAct extends AppCompatActivity
 		{
 			actionBar.setDisplayHomeAsUpEnabled(true);
 		}
+		ImageView img_blog = findViewById(R.id.img_blog);
+		ImageView img_linkedin = findViewById(R.id.img_linkedin);
+		img_blog.setOnClickListener(img_click("blog"));
+		img_linkedin.setOnClickListener(img_click("linkedin"));
+	}
+
+	private View.OnClickListener img_click(String where)
+	{
+		return view -> {
+			if (where.equals("blog"))
+			{
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://numankaraaslan.com")));
+			}
+			else if (where.equals("linkedin"))
+			{
+				startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.linkedin.com/in/numan-karaaslan-1053b9b3")));
+			}
+		};
 	}
 
 	@Override public boolean onOptionsItemSelected(MenuItem item)
